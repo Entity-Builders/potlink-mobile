7036689eb98ea201ba37735581068eabd6cb23a1
juan obrach <juanobrach@gmail.com>
2026-03-01 20:45:16 -0300

    chore(mobile): bump potlink version to v1.0.5

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ module.exports = {
   expo: {
     name,
     slug: 'potlink-mobile',
-    version: '1.0.4',
+    version: '1.0.5',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
